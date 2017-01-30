a46a5655176d568adac4b0374fc20b11c91908f5
Andrés Lou <feasinde@gmail.com>
2017-01-29 22:17:07 -0500

    removing clutter files

--- test_script.py
+++
@@ -1,9 +0,0 @@
-from LexicalAnalyser import Lexer
-
-source = "estas son palabras "
-analyser = Lexer(source)
-c = ""
-while not c == None:
-	c = analyser.nextToken()
-	if not c == None:
-		print(c)
